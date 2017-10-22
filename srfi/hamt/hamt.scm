2d1c6ebf9a4d3c6f35a7141661499cec6814c5fc
Arthur A. Gleckler <srfi@speechcode.com>
2017-10-21 20:44:17 -0700

    Remove MIT Scheme (declare (usual-integrations)).

--- srfi/hamt/hamt.scm
+++ srfi/hamt/hamt.scm
@@ -22,8 +22,6 @@
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ;; DEALINGS IN THE SOFTWARE.
 
-(declare (usual-integrations))
-
 ;;; Naming conventions:
 
 ;;    =: procedure that compares keys
