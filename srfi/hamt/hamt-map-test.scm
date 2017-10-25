e694ca04d517e47cbd6221b81c841fe278c5c13d
Arthur A. Gleckler <srfi@speechcode.com>
2017-10-24 21:02:37 -0700

    Eliminate "out of order define" warnings.

--- srfi/hamt/hamt-map-test.scm
+++ srfi/hamt/hamt-map-test.scm
@@ -23,27 +23,12 @@
 ;; DEALINGS IN THE SOFTWARE.
 
 (define (run-hamt-map-tests)
-  (test-begin "hamt-map")
 
   (define (assert-phm= phm alist)
     (test-equal = (length alist) (phm/count phm))
     (do-list (a alist)
       (test-assert (phm/contains? phm (car a)))
       (test-assert (= (cdr a) (phm/get phm (car a) #f)))))
-
-  (test-group "(persistent-hash-map make-phm alist)"
-    (let* ((alist '(("a" . 1) ("b" . 2)))
-	   (phm (make-phm string-hash string=? alist)))
-      (test-assert (not (hamt/mutable? phm)))
-      (assert-phm= phm alist)))
-
-  (test-group "(persistent-hash-map make-phm phm/count)"
-    (let ((phm (make-phm string-hash string=? '(("a". 1) ("b" . 2)))))
-      (test-assert (= 2 (phm/count phm)))))
-
-  (test-group "(persistent-hash-map phm/empty?)"
-    (test-assert (phm/empty? (make-phm string-hash string=?)))
-    (test-assert (not (phm/empty? (make-phm string-hash string=? '(("a")))))))
 
   (define (phm-random-test put remove transform)
     (define (sort-alist alist)
@@ -92,31 +77,6 @@
 	  (test-assert (equal? (sort-alist ht-alist)
 			       (sort-alist phm-alist)))))))
 
-  (test-group "(persistent-hash-map random pure)"
-    (phm-random-test phm/put phm/remove (lambda (m) m)))
-
-  (test-group "(persistent-hash-map random mutate)"
-    (phm-random-test phm/put! phm/remove! phm/mutable))
-
-  (test-group "(persistent-hash-map random mixed)"
-    (define (flip mutate? phm)
-      ((if mutate? phm/mutable phm/immutable) phm))
-    (phm-random-test (let ((mutate? #t))
-		       (lambda (phm key datum)
-			 (set! mutate? (not mutate?))
-			 ((if mutate? phm/put! phm/put)
-			  (flip mutate? phm)
-			  key
-			  datum)))
-		     (let ((count 0))
-		       (lambda (phm key)
-			 (set! count (remainder (+ count 1) 3))
-			 (let ((mutate? (zero? count)))
-			   ((if mutate? phm/remove! phm/remove)
-			    (flip mutate? phm)
-			    key))))
-		     (lambda (m) m)))
-
   (define (phm-remove-non-existent-test remove transform)
     (define (terrible-hash string) 0)
     (let ((phm (remove (transform (make-phm string-hash string=?))
@@ -131,23 +91,6 @@
       (test-assert (= 1 (phm/count phm)))
       (test-assert (phm/contains? phm "foo"))
       (test-assert (not (phm/contains? phm "not-present")))))
-
-  (test-group "(persistent-hash-map remove-non-existent pure)"
-    (phm-remove-non-existent-test phm/remove (lambda (m) m)))
-
-  (test-group "(persistent-hash-map remove-non-existent mutate)"
-    (phm-remove-non-existent-test phm/remove! phm/mutable))
-
-  (test-group "(persistent-hash-map phm/add-alist)"
-    (let* ((alist '(("foo" . 1) ("bar" . 2) ("baz" . 3)))
-	   (phm (phm/add-alist (make-phm string-hash string=?) alist)))
-      (assert-phm= phm alist)))
-
-  (test-group "(persistent-hash-map phm/add-alist!)"
-    (let* ((alist '(("foo" . 1) ("bar" . 2) ("baz" . 3)))
-	   (phm (phm/mutable (make-phm string-hash string=?))))
-      (phm/add-alist! phm alist)
-      (assert-phm= phm alist)))
 
   (define (phm-collision-test put remove transform)
     (define (sort-alist alist)
@@ -174,6 +117,104 @@
 	  (let ((alist-minus-bat (del-assoc "bat" alist-minus-baz))
 		(phm-minus-bat (remove phm-minus-nonexistent "bat")))
 	    (assert-phm= phm-minus-bat alist-minus-bat))))))
+
+  (define (persistent-hash-map replace transform)
+    (define (sort-alist alist)
+      (sort alist (lambda (a1 a2) (string<? (car a1) (car a2)))))
+    (let* ((alist-1 '(("a" . 1) ("b" . 2) ("c" . 3)))
+	   (alist-2 '(("a" . 1) ("b" . 4) ("c" . 3)))
+	   (alist-3 '(("a" . 1) ("b" . 4)))
+	   (phm (replace (transform (make-phm string-hash string=? alist-1))
+			 "b"
+			 (lambda (x) 4))))
+      (test-assert (equal? alist-2 (sort-alist (phm->alist phm))))
+      (test-assert (equal? alist-3
+			   (sort-alist
+			    (phm->alist
+			     (replace phm "c" (lambda (x) hamt-null))))))))
+
+  (define (hamt-max-depth hamt)
+    "Return maximum depth of `hamt'.  For testing."
+    (let descend ((n (hamt/root hamt)))
+      (cond ((collision? n) 1)
+	    ((narrow? n)
+	     (let* ((array (narrow/array n))
+		    (stride (leaf-stride (hamt/payload? hamt)))
+		    (start (* stride (bit-count (narrow/leaves n))))
+		    (end (vector-length array)))
+	       (do ((i start (+ i 1))
+		    (high 0 (max high (descend (vector-ref array i)))))
+		   ((= i end) (+ high 1)))))
+	    ((wide? n)
+	     (let ((array (wide/array n))
+		   (c (wide/children n)))
+	       (let next-child ((high 0)
+				(i 0))
+		 (cond ((next-set-bit c i hamt-bucket-size)
+			=> (lambda (j)
+			     (next-child (max high
+					      (descend (vector-ref array j)))
+					 (+ j 1))))
+		       (else (+ high 1))))))
+	    (else (error "Invalid type of node." n)))))
+
+  (test-begin "hamt-map")
+
+  (test-group "(persistent-hash-map make-phm alist)"
+    (let* ((alist '(("a" . 1) ("b" . 2)))
+	   (phm (make-phm string-hash string=? alist)))
+      (test-assert (not (hamt/mutable? phm)))
+      (assert-phm= phm alist)))
+
+  (test-group "(persistent-hash-map make-phm phm/count)"
+    (let ((phm (make-phm string-hash string=? '(("a". 1) ("b" . 2)))))
+      (test-assert (= 2 (phm/count phm)))))
+
+  (test-group "(persistent-hash-map phm/empty?)"
+    (test-assert (phm/empty? (make-phm string-hash string=?)))
+    (test-assert (not (phm/empty? (make-phm string-hash string=? '(("a")))))))
+
+  (test-group "(persistent-hash-map random pure)"
+    (phm-random-test phm/put phm/remove (lambda (m) m)))
+
+  (test-group "(persistent-hash-map random mutate)"
+    (phm-random-test phm/put! phm/remove! phm/mutable))
+
+  (test-group "(persistent-hash-map random mixed)"
+    (define (flip mutate? phm)
+      ((if mutate? phm/mutable phm/immutable) phm))
+    (phm-random-test (let ((mutate? #t))
+		       (lambda (phm key datum)
+			 (set! mutate? (not mutate?))
+			 ((if mutate? phm/put! phm/put)
+			  (flip mutate? phm)
+			  key
+			  datum)))
+		     (let ((count 0))
+		       (lambda (phm key)
+			 (set! count (remainder (+ count 1) 3))
+			 (let ((mutate? (zero? count)))
+			   ((if mutate? phm/remove! phm/remove)
+			    (flip mutate? phm)
+			    key))))
+		     (lambda (m) m)))
+
+  (test-group "(persistent-hash-map remove-non-existent pure)"
+    (phm-remove-non-existent-test phm/remove (lambda (m) m)))
+
+  (test-group "(persistent-hash-map remove-non-existent mutate)"
+    (phm-remove-non-existent-test phm/remove! phm/mutable))
+
+  (test-group "(persistent-hash-map phm/add-alist)"
+    (let* ((alist '(("foo" . 1) ("bar" . 2) ("baz" . 3)))
+	   (phm (phm/add-alist (make-phm string-hash string=?) alist)))
+      (assert-phm= phm alist)))
+
+  (test-group "(persistent-hash-map phm/add-alist!)"
+    (let* ((alist '(("foo" . 1) ("bar" . 2) ("baz" . 3)))
+	   (phm (phm/mutable (make-phm string-hash string=?))))
+      (phm/add-alist! phm alist)
+      (assert-phm= phm alist)))
 
   (test-group "(persistent-hash-map collisions pure)"
     (phm-collision-test phm/put phm/remove (lambda (m) m)))
@@ -264,21 +305,6 @@ correctly."
 		    phm)
       (test-assert (equal? alist (sort-alist accumulator)))))
 
-  (define (persistent-hash-map replace transform)
-    (define (sort-alist alist)
-      (sort alist (lambda (a1 a2) (string<? (car a1) (car a2)))))
-    (let* ((alist-1 '(("a" . 1) ("b" . 2) ("c" . 3)))
-	   (alist-2 '(("a" . 1) ("b" . 4) ("c" . 3)))
-	   (alist-3 '(("a" . 1) ("b" . 4)))
-	   (phm (replace (transform (make-phm string-hash string=? alist-1))
-			 "b"
-			 (lambda (x) 4))))
-      (test-assert (equal? alist-2 (sort-alist (phm->alist phm))))
-      (test-assert (equal? alist-3
-			   (sort-alist
-			    (phm->alist
-			     (replace phm "c" (lambda (x) hamt-null))))))))
-
   (test-group "(persistent-hash-map phm/replace)"
     (persistent-hash-map phm/replace (lambda (m) m)))
 
@@ -311,31 +337,6 @@ correctly."
 	   (phm-1 (make-phm terrible-hash string=? alist))
 	   (phm-2 (phm/put phm-1 "baz" 3)))
       (assert-phm= phm-2 '(("foo" . 1) ("bar" . 2) ("baz" . 3)))))
-
-  (define (hamt-max-depth hamt)
-    "Return maximum depth of `hamt'.  For testing."
-    (let descend ((n (hamt/root hamt)))
-      (cond ((collision? n) 1)
-	    ((narrow? n)
-	     (let* ((array (narrow/array n))
-		    (stride (leaf-stride (hamt/payload? hamt)))
-		    (start (* stride (bit-count (narrow/leaves n))))
-		    (end (vector-length array)))
-	       (do ((i start (+ i 1))
-		    (high 0 (max high (descend (vector-ref array i)))))
-		   ((= i end) (+ high 1)))))
-	    ((wide? n)
-	     (let ((array (wide/array n))
-		   (c (wide/children n)))
-	       (let next-child ((high 0)
-				(i 0))
-		 (cond ((next-set-bit c i hamt-bucket-size)
-			=> (lambda (j)
-			     (next-child (max high
-					      (descend (vector-ref array j)))
-					 (+ j 1))))
-		       (else (+ high 1))))))
-	    (else (error "Invalid type of node." n)))))
 
   (test-group "(persistent-hash-map lower-collision)"
     (define same-bottom-three-fragments (expt hamt-bucket-size 3))
